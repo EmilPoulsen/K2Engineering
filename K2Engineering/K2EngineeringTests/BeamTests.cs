d0a2cd565ea03fd5fc99bb70a5d6ce9ba8b14271
Emil Poulsen <emil-poulsen@hotmail.com>
2017-10-22 05:59:20 +0200

    adding unit test project

---
+++ K2Engineering/K2EngineeringTests/BeamTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using K2Engineering;
+using Rhino.Geometry;
+
+namespace K2EngineeringTests {
+    [TestClass]
+    public class BeamTests {
+        [TestMethod]
+        public void CanCalculateMomentCompA() {
+            
+
+            Vector3d vec = new Vector3d(1,0,0);
+            Vector3d yax = new Vector3d(0,1,0);
+            Vector3d xax = new Vector3d(0,0,1);
+            double L = 2;
+
+
+
+            double mz = 1000;
+            double mx = 1000;
+            double my = 1000;
+
+
+            var comp = BeamGoal.CalculateMomentCompA(vec, mx, yax, my, xax, L);
+            //var comp = BeamGoal.CalculateMomentCompB(mz, yax, my, L);
+
+            
+
+
+
+
+        }
+    }
+}

c162f5a419bf9d1d568d179a399e89cd7f906fe5
Emil Poulsen <emil-poulsen@hotmail.com>
2017-10-25 21:19:01 -0400

    normans additions at hackathon
    not sure what it does.. just committing this

--- K2Engineering/K2Engineering/Beam.cs
+++ K2Engineering/K2Engineering/Beam.cs
@@ -37,7 +37,7 @@ namespace K2Engineering {
         /// Registers all the output parameters for this component.
         /// </summary>
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager) {
-            pManager.AddGenericParameter("B", "Bar", "Bar element with force and stress output", GH_ParamAccess.item);
+            pManager.AddGenericParameter("B", "Beam", "Beam element with force and stress output", GH_ParamAccess.item);
             
 
         }
@@ -69,7 +69,7 @@ namespace K2Engineering {
            
             //Create instance of bar
             //GoalObject barElement = new BeamGoal(startPlane, endPlane, L, E, A, Ix, Iy, GJ);
-            GoalObject beamElement = new BeamGoal(startPlane, endPlane, startPlane, endPlane, E, A, Ix, Iy, G, J);
+            GoalObject beamElement = new BeamGoal(startPlane, endPlane, new Plane(startPlane.Origin, Vector3d.XAxis, Vector3d.YAxis), new Plane(endPlane.Origin, Vector3d.XAxis, Vector3d.YAxis), E, A, Ix, Iy, G, J);
 
             //Output
             DA.SetData(0, beamElement);
@@ -81,9 +81,7 @@ namespace K2Engineering {
         
     }
 
-
-    public class BeamGoal : GoalObject
-    {
+    public class BeamGoal : GoalObject {
 
         public Plane P0; //Original plane at first node
         public Plane P1; //Original plane at second node
@@ -95,6 +93,8 @@ namespace K2Engineering {
         public double Iy; //SecondMomentOfAreaAroundYY
         public double G; // ShearModulus;
         public double J; // MomentOfInertiaInTorsion
+        private double _scale;
+
 
         private Vector3d F1, F2, M1, M2;
 
@@ -102,10 +102,14 @@ namespace K2Engineering {
         public double A, GJ, L0;
         public double TX1, TX2, TY1, TY2, twist;
 
-        public BeamGoal(Plane StartPlane, Plane EndPlane, Plane StartNode, Plane EndNode, 
-            double E, double A, double Ix, double Iy, double G, double J)
 
-        {
+        
+        public BeamGoal() {
+            _scale = 1;
+        }
+
+        public BeamGoal(Plane StartPlane, Plane EndPlane, Plane StartNode, Plane EndNode,
+          double E, double A, double Ix, double Iy, double G, double J) :this() {
             this.P0 = StartPlane;
             this.P1 = EndPlane;
 
@@ -119,69 +123,65 @@ namespace K2Engineering {
             this.Iy = Iy;
             this.GJ = G * J;
 
-
-            PPos = new Point3d[2] {StartPlane.Origin, EndPlane.Origin};
+            PPos = new Point3d[2] { StartPlane.Origin, EndPlane.Origin };
             Move = new Vector3d[2];
-            Weighting = new double[2] {1, 1};
+            Weighting = new double[2] { 1 / _scale, 1 / _scale };
             Torque = new Vector3d[2];
-            TorqueWeighting = new double[2] {1, 1};
+            TorqueWeighting = new double[2] { 1 / _scale, 1 / _scale };
 
-            InitialOrientation = new Plane[2] {StartNode, EndNode};
+            InitialOrientation = new Plane[2] { StartNode, EndNode };
 
             P0R = StartPlane;
             P1R = EndPlane;
         }
 
-        
-      	public double CalculateTheta(Vector3d p, Vector3d x)
-    		{
-      		p.Unitize();
-      		return p * x;
-    		}
+        public double CalculateTheta(Vector3d p, Vector3d x) {
+            p.Unitize();
+            return p * x;
+        }
 
-    		public double CalculateTwist(Vector3d x1, Vector3d x2, Vector3d y1, Vector3d y2)
-    		{
-      		return (x1 * y2 - x2 * y1) * 0.5;
-    		}
+        public double CalculateTwist(Vector3d x1, Vector3d x2, Vector3d y1, Vector3d y2) {
+            return (x1 * y2 - x2 * y1) * 0.5;
+        }
 
-    		public double CalculateElongation(Vector3d p, double L0, double Tx1, double Tx2, double Ty1, double Ty2)
-    		{
-      		return (p * p - L0 * L0) / (2 * L0) + L0 / 60 * (4 * (Tx1 * Tx1 + Ty1 * Ty1) - 2 * (Tx1 * Tx2 - Ty1 * Ty2) + 4 * (Tx2 * Tx2 + Ty2 * Ty2));
-    		}
+        public double CalculateElongation(Vector3d p, double L0, double Tx1, double Tx2, double Ty1, double Ty2) {
+            return (p * p - L0 * L0) / (2 * L0) + L0 / 60 * (4 * (Tx1 * Tx1 + Ty1 * Ty1) - 2 * (Tx1 * Tx2 - Ty1 * Ty2) + 4 * (Tx2 * Tx2 + Ty2 * Ty2));
+        }
 
-    		public double CalculateN(double E, double A, double L0, double e)
-    		{
-      		return E * A / L0 * e;
-    		}
+        public double CalculateN(double E, double A, double L0, double e) {
+            return E * A / L0 * e;
+        }
 
-    		public double CalculateM(double N, double L0, double Tx1, double Tx2)
-    		{
-      		return N * L0 / 30 * (4 * Tx1 - Tx2) + 2 * E * Ix / L0 * (2 * Tx1 + Tx2);
-    		}
+        public double CalculateM(double N, double L0, double Tx1, double Tx2) {
+            return N * L0 / 30 * (4 * Tx1 - Tx2) + 2 * E * Ix / L0 * (2 * Tx1 + Tx2);
+        }
 
-    		public double CalculateMT(double G, double J, double L0, double Tz)
-    		{
-      		return G * J * Tz / L0;
-    		}
+        public double CalculateMT(double G, double J, double L0, double Tz) {
+            return G * J * Tz / L0;
+        }
 
-    		public Vector3d CalculateForceAtNode1(double N, Vector3d p, double Mx1, Vector3d y1, double My1, Vector3d x1, double Mx2, Vector3d y2, double My2, Vector3d x2, double L0)
-    		{
-      		return (N * p + Mx1 * y1 - My1 * x1 + Mx2 * y2 + My2 * x2) / L0;
-    		}
+        public Vector3d CalculateForceAtNode1(double N, Vector3d p, double Mx1, Vector3d y1, double My1, Vector3d x1,
+          double Mx2, Vector3d y2, double My2, Vector3d x2, double L0) {
+            return (N * p + Mx1 * y1 - My1 * x1 + Mx2 * y2 + My2 * x2) / L0;
+        }
 
-    		private Vector3d CalculateMomentCompA(Vector3d p, double Mx, Vector3d y, double My, Vector3d x, double L0)
-    		{
-      		return Mx * Vector3d.CrossProduct(y, p) / L0 - My * Vector3d.CrossProduct(x, p) / L0;
-    		}
+        private Vector3d CalculateMomentCompA(Vector3d p, double Mx, Vector3d y, double My, Vector3d x, double L0) {
+            return Mx * Vector3d.CrossProduct(y, p) / L0 - My * Vector3d.CrossProduct(x, p) / L0;
+        }
 
-    		private Vector3d CalculateMomentCompB(double MT, Vector3d y1, Vector3d x1, Vector3d y2, Vector3d x2)
-    		{
-      		return MT * (Vector3d.CrossProduct(x1, y2) - Vector3d.CrossProduct(y1, x2));
-    		}
+        private Vector3d CalculateMomentCompB(double MT, Vector3d y1, Vector3d x1, Vector3d y2, Vector3d x2) {
+            return MT * (Vector3d.CrossProduct(x1, y2) - Vector3d.CrossProduct(y1, x2));
+        }
 
-        public override void Calculate(List<KangarooSolver.Particle> p)
-        {
-        	  //get the current positions/orientations of the nodes
+        private double N;
+        private double Mx1;
+        private double Mx2;
+        private double My1;
+        private double My2;
+        private double Mz;
+
+        public override void Calculate(List<KangarooSolver.Particle> p) {
+            //get the current positions/orientations of the nodes
             Plane NodeACurrent = p[PIndex[0]].Orientation;
             Plane NodeBCurrent = p[PIndex[1]].Orientation;
 
@@ -191,53 +191,108 @@ namespace K2Engineering {
             //..and transform them to get the current beam end frames
             P0R.Transform(Transform.PlaneToPlane(Plane.WorldXY, NodeACurrent));
             P1R.Transform(Transform.PlaneToPlane(Plane.WorldXY, NodeBCurrent));
-            
+
             //Find local angle changes
             Vector3d x1 = P0R.XAxis;
             Vector3d x2 = P1R.XAxis;
             Vector3d y1 = P0R.YAxis;
             Vector3d y2 = P1R.YAxis;
-            
+
             Vector3d Current = P1R.Origin - P0R.Origin;
             double Ty1 = CalculateTheta(Current, x1);
             double Tx1 = CalculateTheta(Current, y1);
             double Ty2 = CalculateTheta(Current, x2);
             double Tx2 = CalculateTheta(Current, y2);
             double Tz = CalculateTwist(x1, x2, y1, y2);
-            
+
+           /////////////////////////////////////////////////////////////////////////////////////////
+
+
+            double CurrentLength = Current.Length;
+            double Stretch = CurrentLength - L0;
+            Vector3d AxialMove = 0.5 * (Current / CurrentLength) * Stretch;
+
+            Vector3d X1 = P0R.XAxis;
+            Vector3d Y1 = P0R.YAxis;
+            Vector3d X2 = P1R.XAxis;
+            Vector3d Y2 = P1R.YAxis;
+
+            //bend angles
+            Vector3d UnitCurrent = Current;
+            UnitCurrent.Unitize();
+            TX1 = Y1 * UnitCurrent;
+            TX2 = Y2 * UnitCurrent;
+            TY1 = X1 * UnitCurrent;
+            TY2 = X2 * UnitCurrent;
+
+            //twist
+            twist = ((X1 * Y2) - (X2 * Y1)) / 2.0;
+
+            //moments
+            Vector3d Moment1 = (X1 * TX1) - (Y1 * TY1);
+            Vector3d Moment2 = (X2 * TX2) - (Y2 * TY2);
+
+            Torque[0] = -0.25 * (Moment1 + twist * Current);
+            Torque[1] = -0.25 * (Moment2 - twist * Current);
+            TorqueWeighting[0] = TorqueWeighting[1] = E * A;
+
+            //  shears
+            Vector3d SY1 = 0.25 * Vector3d.CrossProduct(TX1 * X1, Current);
+            Vector3d SX1 = 0.25 * Vector3d.CrossProduct(TY1 * Y1, Current);
+            Vector3d SY2 = 0.25 * Vector3d.CrossProduct(TX2 * X2, Current);
+            Vector3d SX2 = 0.25 * Vector3d.CrossProduct(TY2 * Y2, Current);
+
+            Move[0] = AxialMove + SX1 - SY1 + SX2 - SY2;
+            Move[1] = -Move[0];
+
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
             //Determine member forces
             double e = CalculateElongation(Current, L0, Tx1, Tx2, Ty1, Ty2);
-            double N = CalculateN(E, A, L0, e);
-            double Mx1 = CalculateM(N, L0, Tx1, Tx2);
-            double Mx2 = CalculateM(N, L0, Tx2, Tx1);
-            double My1 = CalculateM(N, L0, Ty1, Ty2);
-            double My2 = CalculateM(N, L0, Ty2, Ty1);
-            double Mz = CalculateMT(G, J, L0, Tz);
-            
+            N = CalculateN(E, A, L0, e);
+            Mx1 = CalculateM(N, L0, Tx1, Tx2);
+            Mx2 = CalculateM(N, L0, Tx2, Tx1);
+            My1 = CalculateM(N, L0, Ty1, Ty2);
+            My2 = CalculateM(N, L0, Ty2, Ty1);
+            Mz = CalculateMT(G, J, L0, Tz);
+
             //Determine end point forces
-            F1 = CalculateForceAtNode1(N, Current, Mx1, y1, My1, x1, Mx2, y2, My2, x2, L0);
-            F2 = -1 * F1;
-                        
-            Vector3d MCompB = CalculateMomentCompB(Mz, y1, x1, y2, x2);
-            M1 = - (CalculateMomentCompA(Current, Mx1, y1, My1, x1, L0) + MCompB);
-            M2 = - (CalculateMomentCompA(Current, Mx2, y2, My2, x2, L0) + MCompB);
-            
+            //F1 = CalculateForceAtNode1(N, Current, Mx1, y1, My1, x1, Mx2, y2, My2, x2, L0);
+            //F2 = -1 * F1;
+
+            //Vector3d MCompB = CalculateMomentCompB(Mz, y1, x1, y2, x2);
+            //M1 = -(CalculateMomentCompA(Current, Mx1, y1, My1, x1, L0) + MCompB);
+            //M2 = -(CalculateMomentCompA(Current, Mx2, y2, My2, x2, L0) + MCompB);
+
             //Move
-            Move[0] = F1;
-            Move[1] = F2;
-            Torque[0] = M1;
-            Torque[1] = M2;
-          }
-        public override object Output(List<KangarooSolver.Particle> p)
-        {
+            //Move[0] = F1 * _scale;
+            //Move[1] = F2 * _scale;
+            //Torque[0] = M1 * _scale;
+            //Torque[1] = M2 * _scale;
+            //Move[0] = AxialMove + SX1 - SY1 + SX2 - SY;
+            //Move[1] = -Move[0];
+
+            //Weighting
+            //Weighting[0] = F1.Length;
+            //Weighting[1] = F2.Length;
+            //TorqueWeighting[0] = M1.Length;
+            //TorqueWeighting[1] = M2.Length;
+        }
+        public override object Output(List<KangarooSolver.Particle> p) {
             List<object> DataOut = new List<object>();
 
-            DataOut.Add(F1);
-            DataOut.Add(F2);
-            DataOut.Add(M1);
-            DataOut.Add(M2);
-            DataOut.Add(this.P0R);
-            DataOut.Add(this.P1R);
+            DataOut.Add(P0R);
+            DataOut.Add(P1R);
+            DataOut.Add(N);
+            DataOut.Add(Mz);
+            DataOut.Add(Mx1);
+            DataOut.Add(My1);
+            DataOut.Add(Mx2);
+            DataOut.Add(My2);
 
             return DataOut;
         }
